b5841d0cdeab008ed1cf7914ff137217fcae4c59
vocoxo9 <vocoxo9@gmail.com>
2025-06-02 18:13:40 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import ExerciseInputInfo from 'pages/recommend/exercise/ExerciseInputInfo/ExerciseInputInfo';
 
 function App() {
     return (
@@ -13,7 +12,6 @@ function App() {
                 <Header />
 
                 <div className={styles.layout}>
-                    <ExerciseInputInfo/>
                 </div>
 
                 <Footer />
