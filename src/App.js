b09b700c917de470c06bc5a3504d3f40da48869e
vocoxo9 <vocoxo9@gmail.com>
2025-06-02 23:28:22 +0900

    exercise-pages css update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
+import ExerciseInputInfo from 'pages/recommend/exercise/ExerciseInputInfo/ExerciseInputInfo';
+import ExerciseRepeatsDay from 'pages/recommend/exercise/ExerciseRepeatsDay/ExerciseRepeatsDay';
+import RecommendExercise from 'pages/recommend/exercise/RecommendExercise/RecommendExercise';
 
 function App() {
     return (
